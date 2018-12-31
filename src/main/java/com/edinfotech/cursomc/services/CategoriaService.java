0669c847cfcd575dff1952438f32aecc08137673
Edivanio Vieira <edinfosuporte@gmail.com>
2018-12-31 16:11:47 -0200

    atualizando projeto

--- src/main/java/com/edinfotech/cursomc/services/CategoriaService.java
+++ src/main/java/com/edinfotech/cursomc/services/CategoriaService.java
@@ -1,12 +1,18 @@
 package com.edinfotech.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.edinfotech.cursomc.domain.Categoria;
 import com.edinfotech.cursomc.repositories.CategoriaRepository;
+import com.edinfotech.cursomc.services.exceptions.DataIntegrityException;
 import com.edinfotech.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -28,5 +34,20 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	public void delete(Integer id) {
+		find(id);
+		try {
+		repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Nao é possivel excluir uma categoria que contem produtos");
+		}
+	}
+	public List<Categoria> findAll(){
+		return repo.findAll();
+	}
+	public Page<Categoria> findPage(Integer page,Integer linesPerPage,String orderBy,String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
 	}
 }
